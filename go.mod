5454ede967bbf05807db2dc517e0bd7bf200d590
Hugh Hoang <hugh.hoang@onfinality.io>
2022-07-27 23:54:49 +1200

    chore: add local debug server

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/hugh-onf/serverless-functions
 
 go 1.18
 
-require github.com/aws/aws-lambda-go v1.33.0 // indirect
+require github.com/aws/aws-lambda-go v1.33.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/hugh-onf/serverless-functions/api"
+)
+
+var port = 43210
+
+func main() {
+	http.HandleFunc("/api/api_service_dev", api.ApiServiceDev)
+	fmt.Printf("Server is listening at :%d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
